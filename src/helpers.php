4e1f0113504173827205fe87aba595d1e61c53c4
Steve Bauman <steven_bauman@outlook.com>
2015-05-06 11:32:12 -0400

    Update helper documentation

--- src/helpers.php
+++ src/helpers.php
@@ -1,25 +1,30 @@
 <?php
 
-/**
- * Converts a date string into RFC3339 format for google calendar api.
- * This is used for start/end dates for the creation/modification of events.
- * 
- * If $allDay and $isEnd are true, a day is added onto the date 
- * since the Google calendar thinks an all day event that spans
- * multiple days ends on the day before rather than consuming the day it
- * ends on.
- * 
- * Example: For an all day event that starts January 1st and ends January 3rd,
- * Google will end the event on the 2nd since the idea here is that the event
- * is 'till' the 3rd.
- * 
- * @param string $dateStr
- * @param boolean $allDay
- * @param boolean $isEnd
- * @return string
+/*
+ * The Calendar Helpers File.
  */
-if(!function_exists('strToRfc3339')) {
-    
+
+if(! function_exists('strToRfc3339'))
+{
+    /**
+     * Converts a date string into RFC3339 format for google calendar api.
+     * This is used for start/end dates for the creation/modification of events.
+     *
+     * If $allDay and $isEnd are true, a day is added onto the date
+     * since the Google calendar thinks an all day event that spans
+     * multiple days ends on the day before rather than consuming the day it
+     * ends on.
+     *
+     * Example: For an all day event that starts January 1st and ends January 3rd,
+     * Google will end the event on the 2nd since the idea here is that the event
+     * is 'till' the 3rd.
+     *
+     * @param string $dateStr
+     * @param boolean $allDay
+     * @param boolean $isEnd
+     *
+     * @return string
+     */
     function strToRfc3339($dateStr, $allDay = false, $isEnd = false)
     {
         $date = new \DateTime();
@@ -48,15 +53,18 @@ if(!function_exists('strToRfc3339')) {
             return $date->format(\DateTime::RFC3339);
         }
     }
-    
 }
 
-/**
- * Converts a date string into RFC2445 format for google calendar api.
- * This is used for recurrance rule dates
- */
-if(!function_exists('strToRfc2445')) {
-    
+if(! function_exists('strToRfc2445'))
+{
+    /**
+     * Converts a date string into RFC2445 format for google
+     * calendar api. This is used for recurrence rule dates.
+     *
+     * @param $dateStr
+     *
+     * @return string
+     */
     function strToRfc2445($dateStr)
     {
         $date = new \DateTime();
@@ -65,19 +73,21 @@ if(!function_exists('strToRfc2445')) {
 
         return $date->format('Ymd\THis\Z');
     }
-    
 }
 
-/**
- * Checks if the inputted integer timestamp is a valid unix timestamp
- */
-if(!function_exists('isValidTimeStamp')) {
-    
+if(! function_exists('isValidTimeStamp'))
+{
+    /**
+     * Checks if the inputted integer
+     * timestamp is a valid unix timestamp.
+     *
+     * @param $timestamp
+     * @return bool
+     */
     function isValidTimeStamp($timestamp)
     {
         return ((int) $timestamp === $timestamp) 
             && ($timestamp <= PHP_INT_MAX)
             && ($timestamp >= ~PHP_INT_MAX);
     }
-    
 }
